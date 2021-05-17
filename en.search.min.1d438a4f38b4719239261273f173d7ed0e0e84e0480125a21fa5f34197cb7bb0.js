82079637d5126c3901533dca2a3be86eeba93c0b
yewang <yewang@baidu.com>
2021-05-17 22:51:37 +0800

    rebuilding site Mon May 17 22:50:47 CST 2021

--- en.search.min.1d438a4f38b4719239261273f173d7ed0e0e84e0480125a21fa5f34197cb7bb0.js
+++ en.search.min.1d438a4f38b4719239261273f173d7ed0e0e84e0480125a21fa5f34197cb7bb0.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/note-cs/flexsearch.min.js');loadScript('/note-cs/en.search-data.min.a14cfd8d762f351fa6e7eb2cf20f52baa53382f56fd861487735ee90b580284e.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/note-cs/flexsearch.min.js');loadScript('/note-cs/en.search-data.min.46944d4b801e829763fd28867d2147511812cf643e878f98298a4f11bce8e68f.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
